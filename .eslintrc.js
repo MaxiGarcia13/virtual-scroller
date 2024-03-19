3baa7436ebe8e1623cd542484171a4d8c46f4c6b
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2024-03-19 09:27:55 +0100

    fix: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,51 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:storybook/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "vue"
-    ],
-    "rules": {
+  env: {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:storybook/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
     }
-}
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'parser': '@typescript-eslint/parser',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'vue'
+  ],
+  'rules': {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    curly: ['error', 'all'],
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+    indent: ['error', 2],
+    'no-console': 'off',
+    'max-len': ['error', { code: 120, ignoreComments: false }],
+    'no-dupe-class-members': 'off',
+    'no-duplicate-imports': 'error',
+    'no-empty-function': 'off',
+    'no-template-curly-in-string': 'error',
+    'no-unused-expressions': 'off',
+    'no-unused-vars': 'off',
+    'require-atomic-updates': 'error',
+    'require-await': 'error'
+  }
+};

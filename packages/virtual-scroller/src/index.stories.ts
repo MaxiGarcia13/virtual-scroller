a87b1ca25ea29f3203cffba497e56702d5cf9e9d
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2024-03-23 14:45:19 +0100

    feat: add infinity scrolling case

--- packages/virtual-scroller/src/index.stories.ts
+++ packages/virtual-scroller/src/index.stories.ts
@@ -1,38 +1,55 @@
 import { StoryFn, Meta } from '@storybook/vue3';
 import VirtualScroller from './index.component.vue';
+import { ref } from 'vue';
+
+const mapFakeItems = (_:unknown, index:number) => {
+  return {
+    id: `item-id-${index + 1}`
+  };
+};
 
 const generateFakeItems = (num: number): Array<{ id: string }> =>
-  new Array(num).fill('').map((_, i) => {
-    return {
-      id: `item-id-${i + 1}`
-    };
-  });
+  new Array(num).fill('').map(mapFakeItems);
 
 export default {
   title: 'virtual-scroller',
   component: VirtualScroller,
   args: {
     rowHeight: 60,
-    items: generateFakeItems(1000)
+    items: generateFakeItems(1000),
+    distance: 0
   }
 } as Meta<typeof VirtualScroller>;
 
-const Template: StoryFn<typeof VirtualScroller> = (_, { allArgs }) => {
+const Template: StoryFn<typeof VirtualScroller> = (_, { allArgs, loaded }) => {
   return ({
     components: { VirtualScroller },
-    setup: ()=> ({ props: allArgs }),
+    setup: () => {
+      const items = ref(allArgs.items);
+
+      const getMoreItems = () => {
+        items.value = [
+          ...items.value,
+          ...loaded.getMoreItems()
+        ].map(mapFakeItems);
+      };
+
+      return ({ props: allArgs, items, getMoreItems });
+    },
     template: `
-      <VirtualScroller v-bind="props" style="height: 600px; padding: 16px">
+      <VirtualScroller
+        @almostInTheEnd="getMoreItems"
+        v-bind="props"
+        :items="items"
+        style="height: 600px; padding: 16px"
+      >
         <template v-slot="{ itemsToShow }">
           <b style="position: absolute; top: 0; right: 0;">
             Rendered items {{itemsToShow.length}} of {{ props.items.length }}
           </b>
-
-          <ul>
             <li v-for="item in itemsToShow" :key="item.id" style="height: 60px;">
               {{ item.id }}
             </li>
-          </ul>
         </template>
       </VirtualScroller>
     `
@@ -40,3 +57,19 @@ const Template: StoryFn<typeof VirtualScroller> = (_, { allArgs }) => {
 };
 
 export const Vue3VirtualScroller = Template.bind({});
+
+export const Vue3InfinityVirtualScroller = Template.bind({});
+
+const items = generateFakeItems(24);
+
+Vue3InfinityVirtualScroller.args = {
+  rowHeight: 60,
+  items,
+  distance: 300
+};
+
+Vue3InfinityVirtualScroller.loaders = [
+  () => ({
+    getMoreItems: () => generateFakeItems(24),
+  })
+];
